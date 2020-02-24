5fcceb85ec53b3e477140add6dcb1a8406a9faa0
Jonas Eriksson <jonaseriksson84@gmail.com>
2020-02-24 22:19:48 +0100

    Sending emails work

--- functions/contact.js
+++ functions/contact.js
@@ -1,8 +1,26 @@
-exports.handler = (event, _context, callback) => {
-  console.log({ event });
+require("dotenv").config();
 
-  callback(null, {
-    statusCode: 500,
-    body: JSON.stringify({ boop: true })
+exports.handler = (event, _context, callback) => {
+  const mailgun = require("mailgun-js");
+  const mg = mailgun({
+    apiKey: process.env.MAILGUN_API_KEY,
+    domain: process.env.MAILGUN_DOMAIN,
+    host: "api.eu.mailgun.net"
+  });
+
+  const data = JSON.parse(event.body);
+
+  const email = {
+    from: "Jonas Eriksson <jonaseriksson84@gmail.com>",
+    to: `${data.name} <${data.email}>`,
+    subject: data.subject,
+    text: data.body
+  };
+
+  mg.messages().send(email, (error, response) => {
+    callback(error, {
+      statusCode: 200,
+      body: JSON.stringify(response)
+    });
   });
 };
